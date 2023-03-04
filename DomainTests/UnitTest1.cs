b7d11a67fbd6097e972ff6030e0a78d3d93932d8
flacko1703 <proton17031994@gmail.com>
2023-03-04 14:01:28 +0300

    Initial

---
+++ DomainTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace DomainTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ InfrastructureTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace InfrastructureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
