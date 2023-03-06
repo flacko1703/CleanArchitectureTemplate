4e519d955aabbfea03294e130ed4ac3959b43089
flacko1703 <proton17031994@gmail.com>
2023-03-06 10:02:19 +0300

    Host settings, register all services, logging with Serilog package

--- WpfUI/App.xaml.cs
+++ WpfUI/App.xaml.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Serilog;
+using Microsoft.Extensions.Hosting;
+using Application;
+using Infrastructure;
+using Microsoft.Extensions.Logging;
+using Presentation;
 
 namespace WpfUI
 {
@@ -13,5 +12,25 @@ namespace WpfUI
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                //Adding registered dependencies from all layers
+                .ConfigureServices(serviceCollection =>
+                {
+                    serviceCollection.AddApplication();
+                    serviceCollection.AddInfrastructure();
+                    serviceCollection.AddPresentation();
+                })
+                //Logging confuguration
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddSerilog();
+                    logging.SetMinimumLevel(LogLevel.Information);
+                })
+                .UseSerilog()
+                .Build();
+        }
     }
 }
